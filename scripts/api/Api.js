d85dfae06c37009b628d8ab68363d6f6734d05b7
Jonhathan HO TZU YU <hotzuyu@hotzuyu-graphic.fr>
2022-07-12 10:06:24 +0200

    corrections

--- scripts/api/Api.js
+++ scripts/api/Api.js
@@ -4,9 +4,9 @@
  * @description the function getPhotographers is called in the .\scripts\pages\index.js and .\scripts\pages\photographer.js files
  */
 async function getPhotographers() {
-	const photographers = fetch("../data/photographers.json")
+	const photographers = await fetch("../data/photographers.json")
 		.then((res) => res.json())
 		.then((photographers) => photographers)
-		.catch((err) => console.log("Failed to load photographers data" + err));
+		.catch((err) => console.log("Failed to load photographers data " + err));
 	return photographers;
 }
